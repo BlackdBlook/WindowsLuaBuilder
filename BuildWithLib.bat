810bdc82d1a299404943f849c99169149e54b774
BlackBlock <47099583+BlackdBlook@users.noreply.github.com>
2022-10-31 17:53:54 +0800

    Update BuildWithLib.bat

--- BuildWithLib.bat
+++ BuildWithLib.bat
@@ -2,6 +2,5 @@ cl /O2 /W3 /c /DLUA_BUILD_AS_DLL l*.c
 del lua.obj
 link /DLL /out:luaLib.dll l*.obj
 cl /O2 /W3 /c /DLUA_BUILD_AS_DLL lua.c
-link /out:lua.exe lua.obj lua53.lib
-del lua.obj
-link /out:luac.exe l*.obj
+link /out:lua.exe lua.obj luaLib.lib
+del l*.obj
