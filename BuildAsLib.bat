24e30b8b9acaf3cdc665d16cc0de3fc4af142e34
BlackBlock <47099583+BlackdBlook@users.noreply.github.com>
2022-10-31 17:57:41 +0800

    Update and rename BuildWithLib.bat to BuildAsLib.bat

--- BuildAsLib.bat
+++ BuildAsLib.bat
@@ -1,6 +1,23 @@
+@echo off
+
+echo ------ buildObj ------
+
 cl /O2 /W3 /c /DLUA_BUILD_AS_DLL l*.c
+
 del lua.obj
+
+echo ------ linkDll ------
+
 link /DLL /out:luaLib.dll l*.obj
+
+echo ------ buildObj ------
+
 cl /O2 /W3 /c /DLUA_BUILD_AS_DLL lua.c
+
+echo ------ buildExe ------
+
 link /out:lua.exe lua.obj luaLib.lib
+
+echo ------ cleanUp ------
+
 del l*.obj
